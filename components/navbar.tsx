2ba75c7660b063fe72b5fff3fcca7624e8d91878
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2024-03-10 20:24:35 +0530

    initial-v5

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -31,26 +31,7 @@ import {
 
 
 export const Navbar = () => {
-	const searchInput = (
-		<Input
-			aria-label="Search"
-			classNames={{
-				inputWrapper: "bg-default-100",
-				input: "text-sm",
-			}}
-			endContent={
-				<Kbd className="hidden lg:inline-block" keys={["command"]}>
-					K
-				</Kbd>
-			}
-			labelPlacement="outside"
-			placeholder="Search..."
-			startContent={
-				<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-			}
-			type="search"
-		/>
-	);
+
 
 	return (
 		<NextUINavbar maxWidth="xl" position="sticky">
@@ -94,7 +75,7 @@ export const Navbar = () => {
 			</NavbarContent>
 
 			<NavbarMenu>
-				{searchInput}
+				
 				<div className="mx-4 mt-2 flex flex-col gap-2">
 				<UserButton />
 					{siteConfig.navMenuItems.map((item, index) => (
