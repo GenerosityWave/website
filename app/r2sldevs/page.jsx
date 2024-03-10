38d839350fd83ad59e5df7e77155140cb349ae3c
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2024-03-10 22:47:46 +0530

    initial-v12

--- app/r2sldevs/page.jsx
+++ app/r2sldevs/page.jsx
@@ -8,7 +8,7 @@ export default function DocsPage() {
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
       <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
-          <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p>
+          <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Meet the core members of Generosity Wave</p>
       </div> 
       <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
           <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
