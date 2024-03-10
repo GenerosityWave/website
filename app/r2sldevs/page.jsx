98c883de92436bf34242548587ff74d91865cdb2
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2024-03-10 22:46:25 +0530

    photo-added

--- app/r2sldevs/page.jsx
+++ app/r2sldevs/page.jsx
@@ -13,14 +13,14 @@ export default function DocsPage() {
       <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
           <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                  <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Avatar"/>
+                  <img className="w-45 rounded-lg sm:rounded-none sm:rounded-l-lg" src="/images/LAVANJV-AI.png" alt="LAVAN JV"/>
               </a>
               <div className="p-5">
                   <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      <a href="#">Bonnie Green</a>
+                      <a href="#">Lavan J V</a>
                   </h3>
                   <span className="text-gray-500 dark:text-gray-400">CEO & Web Developer</span>
-                  <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Bonnie drives the technical strategy of the flowbite platform and brand.</p>
+                  <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Drives the technical strategy of the  Generosity Waveplatform and brand.</p>
                   <ul className="flex space-x-4 sm:mt-0">
                       <li>
                           <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -47,14 +47,14 @@ export default function DocsPage() {
           </div> 
           <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                  <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Avatar"/>
+                  <img className="h-96 w-96 rounded-lg sm:rounded-none sm:rounded-l-lg" src="/images/Sanjeev.jpg" alt="Sanjeev"/>
               </a>
               <div className="p-5">
                   <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      <a href="#">Jese Leos</a>
+                      <a href="#">Sanjeev Kumar</a>
                   </h3>
                   <span className="text-gray-500 dark:text-gray-400">CTO</span>
-                  <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Jese drives the technical strategy of the flowbite platform and brand.</p>
+                  <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">  Drives the technical strategy of the  Generosity Waveplatform and brand.</p>
                   <ul className="flex space-x-4 sm:mt-0">
                       <li>
                           <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -81,14 +81,14 @@ export default function DocsPage() {
           </div> 
           <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                  <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="Michael Avatar"/>
+                  <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="/images/rohit.png" alt="Rohit prabhakaran"/>
               </a>
               <div className="p-5">
                   <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      <a href="#">Michael Gough</a>
+                      <a href="#">Rohit prabhakaran</a>
                   </h3>
                   <span className="text-gray-500 dark:text-gray-400">Senior Front-end Developer</span>
-                  <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Michael drives the technical strategy of the flowbite platform and brand.</p>
+                  <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400"> Drives the technical strategy of the  Generosity Waveplatform and brand.</p>
                   <ul className="flex space-x-4 sm:mt-0">
                       <li>
                           <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -115,14 +115,14 @@ export default function DocsPage() {
           </div> 
           <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                  <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png" alt="Sofia Avatar"/>
+                  <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="/images/rahulram.png" alt="Rahulram"/>
               </a>
               <div className="p-5">
                   <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      <a href="#">Lana Byrd</a>
+                      <a href="#">Rahulram</a>
                   </h3>
                   <span className="text-gray-500 dark:text-gray-400">Marketing & Sale</span>
-                  <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Lana drives the technical strategy of the flowbite platform and brand.</p>
+                  <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">   Drives the technical strategy of the  Generosity Waveplatform and brand.</p>
                   <ul className="flex space-x-4 sm:mt-0">
                       <li>
                           <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
